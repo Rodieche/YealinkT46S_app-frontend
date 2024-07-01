e560bbf63a0778d8ddbfa7c9bdb9c18a37daeeac
Rudolf Echenique @ Expert I.T <rudolf@expertit.co.nz>
2024-07-01 14:05:07 +1200

    Websocket: working connection with server

--- src/websocket/socket-client.ts
+++ src/websocket/socket-client.ts
@@ -4,20 +4,11 @@ export const connectToServer = (): Socket => {
 	console.log('Socket called')
     const manager = new Manager('http://localhost:3000/socket.io/socket.io.js');
     const socket = manager.socket('/');
-    console.log(socket);
 
-    // socket.on('connect', () => {
-    //     console.log('Connected to server');
-    // });
-
-    // socket.on('disconnect', () => {
-    //     console.log('Disconnected from server');
-    // });
-
-    // // Agrega más eventos según sea necesario
-    // socket.on('someEvent', (data: any) => {
-    //     console.log('Received someEvent with data:', data);
-    // });
+    socket.on('incomingCall', (payload)=> {
+        console.log('Incoming Call');
+        console.log(payload);
+    });
 
     return socket;
 };
